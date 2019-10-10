350913eaf610d2d1ac7573c61acf1a6f4156643d
Peter Irshad <pete.t3141@gmail.com>
2019-10-09 22:50:47 -0400

    Setup bindings for player, added basic movement.
    - Player object created and key bindings have been set up.
    - Player can walk left and right.
    - Player can jump (must add condition where player can't jump
      in midair).

--- Assets/CodeBase/Entities/player/player.cs
+++ Assets/CodeBase/Entities/player/player.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
+    PlayerInputProfile inputProfile;
+    Rigidbody playerRigidBody;
     // Start is called before the first frame update
     void Start()
     {
-        
+        inputProfile = new PlayerInputProfile();
+        playerRigidBody = GetComponent<Rigidbody>();
+
+        inputProfile.addListener(InputEvent.Key, PlayerInputProfile.moveUp, moveUp);
+        inputProfile.addListener(InputEvent.Key, PlayerInputProfile.moveDown, moveDown);
+        inputProfile.addListener(InputEvent.Key, PlayerInputProfile.moveLeft, moveLeft);
+        inputProfile.addListener(InputEvent.Key, PlayerInputProfile.moveRight, moveRight);
+        inputProfile.addListener(InputEvent.Key, PlayerInputProfile.jump, jump);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        inputProfile.checkInput();
+    }
+
+    void moveUp()
+    {
+        //Does nothing for now.
+        //Will influence direction of fire dash and climbing up with earth.
+    }
+
+    void moveDown()
+    {
+        //Does nothing for now.
+        //Will influence direction of fire dash and climbing down with earth.
+    }
+
+    void moveLeft()
+    {
+        transform.Translate(Vector2.left);
+    }
+
+    void moveRight()
+    {
+        transform.Translate(Vector2.right);
+    }
+
+    void jump()
+    {
+        playerRigidBody.AddForce(Vector3.up, ForceMode.Impulse);
     }
 }
