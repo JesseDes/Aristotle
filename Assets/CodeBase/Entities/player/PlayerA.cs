6a91acb00d128ac6741b43cdc1b332a93611e829
Peter Irshad <pete.t3141@gmail.com>
2019-10-11 21:40:19 -0400

    Workaround commit for file rename
    - Case sensitive exclusive changes does not seem to be detected by unity.

---
+++ Assets/CodeBase/Entities/player/PlayerA.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed;
+
+    [SerializeField]
+    float jumpSpeed;
+
+    PlayerInputProfile inputProfile;
+    Rigidbody playerRigidBody;
+
+    private bool _isGrounded;
+    // Start is called before the first frame update
+    void Start()
+    {
+        inputProfile = new PlayerInputProfile();
+        playerRigidBody = GetComponent<Rigidbody>();
+
+        _isGrounded = false;
+
+        inputProfile.addListener(InputEvent.Key, PlayerInputProfile.moveUp, moveUp);
+        inputProfile.addListener(InputEvent.Key, PlayerInputProfile.moveDown, moveDown);
+        inputProfile.addListener(InputEvent.Key, PlayerInputProfile.moveLeft, moveLeft);
+        inputProfile.addListener(InputEvent.Key, PlayerInputProfile.moveRight, moveRight);
+        inputProfile.addListener(InputEvent.Key, PlayerInputProfile.jump, jump);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputProfile.checkInput();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            //Setting the free-fall velocity to 0 prevents boosted jumps at corners.
+            playerRigidBody.velocity = new Vector3(playerRigidBody.velocity.x, 0.0f, playerRigidBody.velocity.z);
+            _isGrounded = true;
+        }
+    }
+
+    void moveUp()
+    {
+        //Does nothing for now.
+        //Will influence direction of fire dash and climbing up with earth.
+        jump(); //Unsure whether the up key or space is a better jump button.
+    }
+
+    void moveDown()
+    {
+        //Does nothing for now.
+        //Will influence direction of fire dash and climbing down with earth.
+    }
+
+    void moveLeft()
+    {
+        transform.Translate(Vector2.left*moveSpeed);
+    }
+
+    void moveRight()
+    {
+        transform.Translate(Vector2.right*moveSpeed);
+    }
+
+    void jump()
+    {
+        if (_isGrounded)
+        {
+            _isGrounded = false;
+            playerRigidBody.AddForce(Vector3.up*jumpSpeed, ForceMode.Impulse);
+        }
+    }
+}
---
+++ Assets/CodeBase/Entities/sphere/SphereA.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Sphere : MonoBehaviour
+{
+    SphereInputProfile inputProfile;
+    // Start is called before the first frame update
+    void Start()
+    {
+        inputProfile = new SphereInputProfile();
+        
+        inputProfile.addListener(InputEvent.Key, SphereInputProfile.moveUp, moveUp);
+        inputProfile.addListener(InputEvent.Key, SphereInputProfile.moveDown, moveDown);
+        inputProfile.addListener(InputEvent.Key, SphereInputProfile.moveLeft, moveLeft);
+        inputProfile.addListener(InputEvent.Key, SphereInputProfile.moveRight, moveRight);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputProfile.checkInput();
+
+    }
+
+    private void moveUp()
+    {
+        transform.Translate(Vector3.up);
+    }
+
+    private void moveDown()
+    {
+        transform.Translate(Vector3.down);
+    }
+
+    private void moveLeft()
+    {
+        transform.Translate(Vector2.left);
+    }
+
+    private void moveRight()
+    {
+        transform.Translate(Vector2.right); 
+    }
+}
