d349e306cd661e6f218decfb7d3916da8f0d829b
Peter Irshad <pete.t3141@gmail.com>
2019-10-15 17:46:58 -0400

    Wind Ability Implemented
    - Basic form of wind has been implemented (still subject to changes in very specific moments such as whether player properties are affected mid-jump or not).
    - Sphere objects and Sphere script have been removed (it has outlived it's purpose).

--- Assets/CodeBase/Entities/player/Player.cs
+++ Assets/CodeBase/Entities/player/Player.cs
@@ -19,6 +19,13 @@ public class Player : MonoBehaviour
     [SerializeField]
     float jumpSpeed;
 
+    private const float NORMAL_MOVEMENT_SPEED = 15.0f;
+    private const float NORMAL_JUMP_SPEED = 5.0f;
+    private const float NORMAL_MASS = 1.0f;
+
+    private const float WIND_JUMP_SPEED = NORMAL_JUMP_SPEED * 1.5f;
+    private const float WIND_MASS = NORMAL_MASS * 0.8f;
+
     ActiveAbility currentAbility;
 
     PlayerInputProfile inputProfile;
@@ -32,6 +39,10 @@ public class Player : MonoBehaviour
         playerRigidBody = GetComponent<Rigidbody2D>();
 
         _isGrounded = false;
+
+        moveSpeed = NORMAL_MOVEMENT_SPEED;
+        jumpSpeed = NORMAL_JUMP_SPEED;
+        playerRigidBody.mass = NORMAL_MASS;
 
         //Listeners for movement and jumping.
         inputProfile.addListener(InputEvent.Key, PlayerInputProfile.moveUp, moveUp);
@@ -53,7 +64,6 @@ public class Player : MonoBehaviour
     void Update()
     {
         inputProfile.checkInput();
-        Debug.Log(currentAbility.ToString());
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -91,22 +101,28 @@ public class Player : MonoBehaviour
 
     void jump()
     {
-        if (_isGrounded)
+        if (_isGrounded) //TODO: Add statement to prevent jumping while ice is active.
         {
             _isGrounded = false;
             playerRigidBody.AddForce(Vector2.up*jumpSpeed, ForceMode2D.Impulse);
         }
     }
 
+    //NOTE: Changing the sprite color is a temporary measure until proper animations are
+    //implemented.
     void toggleIce()
     {
         if (!currentAbility.Equals(ActiveAbility.ICE))
         {
             currentAbility = ActiveAbility.ICE;
+            //May need to add ice constants for these properties.
+            jumpSpeed = NORMAL_JUMP_SPEED;
+            playerRigidBody.mass = NORMAL_MASS;
         }
         else
         {
             currentAbility = ActiveAbility.NORMAL;
+            GetComponent<SpriteRenderer>().color = Color.cyan;
         }
         //TBD this iteration.
     }
@@ -116,6 +132,9 @@ public class Player : MonoBehaviour
         if (!currentAbility.Equals(ActiveAbility.FIRE))
         {
             currentAbility = ActiveAbility.FIRE;
+            GetComponent<SpriteRenderer>().color = Color.red;
+            jumpSpeed = NORMAL_JUMP_SPEED;
+            playerRigidBody.mass = NORMAL_MASS;
         }
         else
         {
@@ -129,6 +148,9 @@ public class Player : MonoBehaviour
         if (!currentAbility.Equals(ActiveAbility.WIND))
         {
             currentAbility = ActiveAbility.WIND;
+            GetComponent<SpriteRenderer>().color = Color.green;
+            jumpSpeed = WIND_JUMP_SPEED;
+            playerRigidBody.mass = WIND_MASS;
         }
         else
         {
@@ -141,6 +163,9 @@ public class Player : MonoBehaviour
         if (!currentAbility.Equals(ActiveAbility.EARTH))
         {
             currentAbility = ActiveAbility.EARTH;
+            GetComponent<SpriteRenderer>().color = Color.yellow;
+            jumpSpeed = NORMAL_JUMP_SPEED;
+            playerRigidBody.mass = NORMAL_MASS;
         }
         else
         {
@@ -151,6 +176,10 @@ public class Player : MonoBehaviour
 
     void deactivateAbility()
     {
+        moveSpeed = NORMAL_MOVEMENT_SPEED; //In anticipation for ice slowing movement down.
+        jumpSpeed = NORMAL_JUMP_SPEED;
+        playerRigidBody.mass = NORMAL_MASS;
         currentAbility = ActiveAbility.NORMAL;
+        GetComponent<SpriteRenderer>().color = Color.white;
     }
 }
