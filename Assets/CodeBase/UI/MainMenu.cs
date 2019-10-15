8451901fde2c9f5e98d0f20ffc9639af2d9624e4
Megan <megan.vanhumbeck@gmail.com>
2019-10-15 13:37:30 -0400

    #9 Main menu set up

---
+++ Assets/CodeBase/UI/MainMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void UI_Start()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    public void UI_Options()
+    {
+        //CLose menu 
+        //Open options
+    }
+}
