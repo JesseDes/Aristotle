b1ed8b7ee1e1c67a059218d98aebfcb71a8f118d
moogey <muigi.rulock@gmail.com>
2019-10-21 00:09:11 -0400

    Made Event Generic Event System, added features to State
    States can now dispatch a generic value
    Generic AEvent Class Added

---
+++ Assets/CodeBase/Data/EngineState.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public enum EngineState
+{
+    MENU = 0,
+    ACTIVE = 1,
+    PLAYER_DEAD = 2, 
+    STAGE_END = 3,
+    CUTSCENES = 4,
+    LOADING_STATE = 5   
+}
---
+++ Assets/CodeBase/Data/State.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+/// <summary>
+/// States are Types of Events where they broadcast their identifiers.
+/// This State will dispatch NO arguments 
+/// </summary>
+/// <typeparam name="T">Type of Identifier </typeparam>
+public class State<T> : AEvent              
+{
+    public T name { get; private set; }
+ 
+    public State(T stateName) 
+    {
+        name = stateName;
+
+    }
+
+}
+
+/// <summary>
+/// States are Types of Events where they broadcast their identifiers
+/// This State will Dispatch an argument (Make a payload class if you need to push multiple values)
+/// </summary>
+/// <typeparam name="T">Type of Identifier </typeparam>
+/// <typeparam name="X"> This is the Value you wish to dispatch</typeparam>
+public class State<T,X> : AEvent
+{
+    public T name { get; private set; }
+
+    readonly X _returnValue;
+
+    public State(T stateName, X returnValue)
+    {
+        name = stateName;
+        _returnValue = returnValue;
+    }
+
+    public void Dispatch()
+    {
+        Dispatch(_returnValue);
+    }
+
+}
