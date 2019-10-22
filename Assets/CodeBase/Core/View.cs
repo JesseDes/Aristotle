16be8d65dd540a3280284a0606b8c9927d0beb06
Megan <megan.vanhumbeck@gmail.com>
2019-10-22 13:53:51 -0400

    #18 Updated font, Added ability overlay

--- Assets/CodeBase/Core/View.cs
+++ Assets/CodeBase/Core/View.cs
@@ -6,6 +6,11 @@ public class View : MonoBehaviour
     public static View instance;
     public int sceneCount = 0;
 
+    [SerializeField]
+    private AbilityOverlay _abilities = default;
+    [SerializeField]
+    private MainMenu _mainMenu = default;
+
     private GameObject HUD;
     private Camera mainCamera;
     public GameObject testFab;
@@ -28,15 +33,35 @@ public class View : MonoBehaviour
         SceneManager.LoadScene(sceneCount);
     }
 
-    // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
+        Controller.instance.stateMachine.AddStateListener(OnStateChange);
         mainCamera = Camera.main;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnDestroy()
     {
-        
+        Controller.instance.stateMachine.RemoveStateListener(OnStateChange);
+    }
+
+    private void OnStateChange(System.Object response)
+    {
+        if (Controller.instance.stateMachine.state == EngineState.MENU)
+        {
+            _abilities.gameObject.SetActive(false);
+            _mainMenu.gameObject.SetActive(true);
+
+        }
+        else if (Controller.instance.stateMachine.state == EngineState.ACTIVE)
+        {
+            _abilities.gameObject.SetActive(true);
+            _mainMenu.gameObject.SetActive(false);
+        }
+
+    }
+
+    public void UpdateAbilitySymbol(ActiveAbility ability, AbilitySymbolState state)
+    {
+        _abilities.UpdateAbilitySymbol(ability, state);
     }
 }
--- Assets/CodeBase/UI/MainMenu.cs
+++ Assets/CodeBase/UI/MainMenu.cs
@@ -4,7 +4,6 @@ public class MainMenu : MonoBehaviour
 {
     public void UI_Start()
     {
-        gameObject.SetActive(false);
         Controller.instance.Dispatch(EngineEvents.ENGINE_GAME_START);
     }
 
