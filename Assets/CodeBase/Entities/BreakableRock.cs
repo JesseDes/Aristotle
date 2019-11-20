bd7a4e1870df61a8bc902ecd53d56c0dfdbce9e2
Megan <megan.vanhumbeck@gmail.com>
2019-11-20 13:53:45 -0500

    Added vfx for rock and fixed colliders on fire cannon

--- Assets/CodeBase/Entities/BreakableRock.cs
+++ Assets/CodeBase/Entities/BreakableRock.cs
@@ -7,10 +7,15 @@ public class BreakableRock : Respawnable
     string playerTag = "Player";
     float resistance = 10f;
 
+    [SerializeField]
+    private GameObject _vfx;
+
     public void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.tag == playerTag && other.gameObject.GetComponent<Player>().currentAbility.Equals(ActiveAbility.ICE)) {
             if (other.gameObject.GetComponent<Rigidbody2D>().velocity.y < -resistance) {
-                //TODO: add visual effects later...
+                GameObject vfx = Instantiate(_vfx, transform);
+                vfx.transform.localPosition = Vector3.zero;
+                vfx.transform.parent = null;
 
                 //break the rock if crashing downwards into it fast enough
                 Deactivate();
