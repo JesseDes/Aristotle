21e0e41f3b0dd792f7b552010a48661a70ecd7b9
joshuajgit <43454615+joshuajgit@users.noreply.github.com>
2019-11-19 23:07:56 -0500

    level 3 fully playable
    - level layout done
    - added laser drone to level 3

---
+++ Assets/CodeBase/Entities/MoveObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour {
+    public float moveDistance;
+    float speed = 0.08f;
+
+    Vector2 startPos;
+    public Vector2 moveDirection;
+
+    // Start is called before the first frame update
+    void Start() {
+        startPos = transform.position;
+        moveDirection = moveDirection.normalized;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        if ((transform.position.x > startPos.x + moveDistance) ||
+            (transform.position.y > startPos.y + moveDistance) ||
+            (transform.position.x < startPos.x - moveDistance) ||
+            (transform.position.y < startPos.y - moveDistance)) {
+            moveDirection *= -1;
+        }
+        transform.Translate(moveDirection * speed);
+    }
+}
