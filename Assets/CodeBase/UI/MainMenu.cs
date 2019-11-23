cff72577aef6e01abdccbde9c1071a5f855aa8ea
Megan <megan.vanhumbeck@gmail.com>
2019-11-23 11:44:33 -0500

    Added Credits to Main Menu

--- Assets/CodeBase/UI/MainMenu.cs
+++ Assets/CodeBase/UI/MainMenu.cs
@@ -8,6 +8,9 @@ public class MainMenu : MonoBehaviour
     [SerializeField]
     private GameObject _optionsMenu;
 
+    [SerializeField]
+    private GameObject _creditsMenu;
+
     public void Start()
     {
         UI_MainMenu();
@@ -22,11 +25,25 @@ public class MainMenu : MonoBehaviour
     {
         _mainMenu.SetActive(false);
         _optionsMenu.SetActive(true);
+        _creditsMenu.SetActive(false);
     }
 
     public void UI_MainMenu()
     {
         _mainMenu.SetActive(true);
         _optionsMenu.SetActive(false);
+        _creditsMenu.SetActive(false);
+    }
+
+    public void UI_Credits()
+    {
+        _mainMenu.SetActive(false);
+        _optionsMenu.SetActive(false);
+        _creditsMenu.SetActive(true);
+    }
+
+    public void UI_Quit()
+    {
+        Application.Quit();
     }
 }
---
+++ Assets/CodeBase/UI/OpenLink.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLink : MonoBehaviour
+{
+    public void UI_OpenLink(string url)
+    {
+        Application.OpenURL(url);
+    }
+}
