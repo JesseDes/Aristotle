c249ec334dee50a04598c8a2fa0bb197ec2d14db
Megan <megan.vanhumbeck@gmail.com>
2019-11-23 12:26:21 -0500

    Update to Lore Triggers

--- Assets/CodeBase/UI/LoreTrigger.cs
+++ Assets/CodeBase/UI/LoreTrigger.cs
@@ -7,12 +7,39 @@ public class LoreTrigger : MonoBehaviour
     [SerializeField]
     private GameObject _canvas;
 
+    private bool _isColliding = false;
+    private PlayerInputProfile inputProfile;
+
+    private void Update()
+    {
+        if (_isColliding)
+            inputProfile.checkInput();
+    }
+
+    public void Start()
+    {
+        inputProfile = new PlayerInputProfile();
+        inputProfile.addListener(InputEvent.Down, PlayerInputProfile.shift, ShowLore);
+        inputProfile.addListener(InputEvent.Up, PlayerInputProfile.shift, HideLore);
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
+    {
+        _isColliding = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        _isColliding = false;
+        HideLore();
+    }
+
+    private void ShowLore()
     {
         _canvas.SetActive(true);
     }
 
-    private void OnTriggerExit2D(Collider2D collision)
+    private void HideLore()
     {
         _canvas.SetActive(false);
     }
